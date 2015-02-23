0b4043d338f7ee94b13432c3afc83bb04e45ccd0
Jason Britton <velograph@gmail.com>
2015-02-23 09:54:28 -0800

    Initial Commit

---
+++ content-page.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template used for displaying page content in page.php
+ *
+ * @package Pieper Cafe
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'Pieper Cafe' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php edit_post_link( __( 'Edit', 'Pieper Cafe' ), '<span class="edit-link">', '</span>' ); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
